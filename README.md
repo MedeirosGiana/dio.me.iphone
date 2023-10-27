d1c0ab232146d9bd95293a29f015def721b95ec4
MedeirosGiana <gianamedeiros.00510@gmail.com>
2023-10-27 08:51:30 -0300

    atualizando diagrama

--- README.md
+++ README.md
@@ -14,9 +14,7 @@ Aparelho Telefônico: ligar, atender, iniciarCorrerioVoz
 Navegador na Internet: exibirPagina, adicionarNovaAba, atualizarPagina
 
 ### Modelagem UML
-    
- 
- https://drive.google.com/file/d/1oQSCXUMVz2JV8oxidum_mbBAROhii6-t/view?usp=sharing
 
+https://drive.google.com/file/d/1oQSCXUMVz2JV8oxidum_mbBAROhii6-t/view?usp=sharing    
 ![img.png](img.png)
 
---
+++ src/entities/IPhone.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet{
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+
+    }
+
+    @Override
+    public void adicionarpagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+}
